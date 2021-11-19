60cb0c501294ba8999d46387b5dadb83ea3e40d9
Andrew Bayer <andrew.bayer@gmail.com>
2021-11-19 11:02:25 -0500

    template.JS-ify
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- report.go
+++ report.go
@@ -1002,7 +1002,7 @@ func GenerateVersionDistributions(db sq.BaseRunner, year, month int, outputDir s
 		}
 		err = tmpl.Execute(outFile, map[string]interface{}{
 			"pluginName":        k,
-			"pluginVersionData": string(versionInfo),
+			"pluginVersionData": template.JS(versionInfo),
 		})
 		if err != nil {
 			return nil, err
